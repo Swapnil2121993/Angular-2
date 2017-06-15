2c46475df57ff2e680520516b6e13a5ec5c17b55
swapnil trivedi <swapnil trivedi>
2017-06-14 23:44:35 -0400

    routing

--- app/course/course-detail.component.js
+++ app/course/course-detail.component.js
@@ -5,19 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var CourseDetailComponent = (function () {
-    function CourseDetailComponent() {
+    function CourseDetailComponent(_route, _router) {
+        this._route = _route;
+        this._router = _router;
         this.pageTitle = "Course Detail";
     }
+    CourseDetailComponent.prototype.ngOnInit = function () {
+        var id = this._route.snapshot.params['id'];
+        this.pageTitle += ": " + id;
+    };
+    CourseDetailComponent.prototype.onBack = function () {
+        this._router.navigate['/courses'];
+    };
     return CourseDetailComponent;
 }());
 CourseDetailComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         templateUrl: 'course-detail.component.html',
-    })
+    }),
+    __metadata("design:paramtypes", [router_1.ActivatedRoute,
+        router_1.Router])
 ], CourseDetailComponent);
 exports.CourseDetailComponent = CourseDetailComponent;
 //# sourceMappingURL=course-detail.component.js.map
--- app/course/course-detail.component.ts
+++ app/course/course-detail.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import{ActivatedRoute, Router} from '@angular/router';
 import {IcourseList} from './course-list';
 
 @Component({
@@ -6,8 +7,20 @@ import {IcourseList} from './course-list';
   templateUrl:'course-detail.component.html',
 })
 
-export class CourseDetailComponent{
+export class CourseDetailComponent implements OnInit{
   pageTitle:string = "Course Detail";
   courses:IcourseList;
+
+  constructor(private _route:ActivatedRoute,
+              private _router:Router){ }
+
+  ngOnInit():void{
+    let id= this._route.snapshot.params['id'];
+    this.pageTitle+=`: ${id}`;
+  }
+
+  onBack():void{
+    this._router.navigate['/courses']
+  }
 
 }
